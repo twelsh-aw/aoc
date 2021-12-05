7f280dfd4ee8422fbce7486e8f8647c560e2fea4
Tim Welsh <tim.welsh@arcticwolf.com>
2021-12-04 23:39:47 -0500

    Cleanup day 4
    Less ugly

--- 2021/04/main.js
+++ 2021/04/main.js
@@ -4,66 +4,63 @@ const path = require('path')
 let inputPath = path.resolve(__dirname, "input.txt");
 let input = fs.readFileSync(inputPath).toString().split("\n");
 let draws = input[0].split(',')
+let boards = parseBoards(input)
 
-let boardsRaw = input.slice(2, input.len).filter(b => b !== '')
-let nums = []
-for (let i = 0; i < boardsRaw.length; i++) {
-    let s = boardsRaw[i].split('\n')
-    for (let j = 0; j < 5; j++) {
-        let num = s.join(' ').trim().split(' ').filter((j => j != ''))[j]
-        nums.push(num)
-    }
-}
+let p1 = puzzle1(boards, draws)
+let p2 = puzzle2(boards, draws)
+console.log(p1, p2)
 
-let rows = []
-let row = []
-for (let i = 0; i < nums.length; i++) {
-    row.push(nums[i])
-    if ((i + 1) % 5 === 0) {
-        rows.push([...row])
-        row = []
-    }
-}
+function puzzle1(boards, draws) {
+    for (let num of draws) {
+        for (let board of boards) {
+            for (let i = 0; i < 5; i++) {
+                for (let j = 0; j < 5; j++) {
+                    if (board[i][j] == num) {
+                        board[i][j] = -1
+                    }
 
-let boards = []
-let board = []
-for (let j = 0; j < rows.length; j++) {
-    board.push(rows[j])
-    if ((j+1) % 5 === 0) {
-        boards.push([...board])
-        board = []
-    }
-}
-
-let winningBoards = []
-
-for (let num of draws) {
-    for (let board of boards) {
-        for (let i = 0; i < 5; i++) {
-            for (let j = 0; j < 5; j++) {
-                if (board[i][j] == num) {
-                    board[i][j] = -1
-                }
-
-                if (checkBoard(board) && !winningBoards.some(b => b == board)) {
-                    console.log(board)
-                    winningBoards.push(board)
-                    if (winningBoards.length === boards.length) {
-                        let sum = 0
-                        for (let i = 0; i < 5; i++) {
-                            for (let j = 0; j < 5; j++) {
-                                if (board[i][j] !== -1) {
-                                    sum += parseInt(board[i][j])
-                                }
-                            }
-                        }
-                        console.log(sum*num)
-                        return
+                    if (checkBoard(board)) {
+                        return scoreBoard(board, num)
                     }
                 }
             }
         }
     }
+}
+
+function puzzle2(boards, draws) {
+    let winningBoards = []
+    for (let num of draws) {
+        for (let board of boards) {
+            for (let i = 0; i < 5; i++) {
+                for (let j = 0; j < 5; j++) {
+                    if (board[i][j] == num) {
+                        board[i][j] = -1
+                    }
+
+                    if (checkBoard(board) && !winningBoards.some(b => b == board)) {
+                        winningBoards.push(board)
+                        if (winningBoards.length === boards.length) {
+                            return scoreBoard(board, num)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+function scoreBoard(board, num) {
+    let sum = 0
+    for (let i = 0; i < 5; i++) {
+        for (let j = 0; j < 5; j++) {
+            if (board[i][j] !== -1) {
+                sum += parseInt(board[i][j])
+            }
+        }
+    }
+
+    return sum * num
 }
 
 function checkBoard(board) {
@@ -107,4 +104,38 @@ function checkBoard(board) {
 
 function transpose(board) {
     return board[0].map((_, colIndex) => board.map(row => row[colIndex]));
+}
+
+function parseBoards(input) {
+    let boardsRaw = input.slice(2, input.len).filter(b => b !== '')
+    let nums = []
+    for (let i = 0; i < boardsRaw.length; i++) {
+        let s = boardsRaw[i].split('\n')
+        for (let j = 0; j < 5; j++) {
+            let num = s.join(' ').trim().split(' ').filter((j => j != ''))[j]
+            nums.push(num)
+        }
+    }
+
+    let rows = []
+    let row = []
+    for (let i = 0; i < nums.length; i++) {
+        row.push(nums[i])
+        if ((i + 1) % 5 === 0) {
+            rows.push([...row])
+            row = []
+        }
+    }
+
+    let boards = []
+    let board = []
+    for (let j = 0; j < rows.length; j++) {
+        board.push(rows[j])
+        if ((j+1) % 5 === 0) {
+            boards.push([...board])
+            board = []
+        }
+    }
+
+    return boards
 }
