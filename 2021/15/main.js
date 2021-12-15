dd10e31a83bdd1d2c0e3d7d35ad1a4e8097e466d
Tim Welsh <tim.welsh@arcticwolf.com>
2021-12-15 11:16:25 -0500

    Fix day 15
    Implement dkijstra's alg from wikipedia

--- 2021/15/main.js
+++ 2021/15/main.js
@@ -2,45 +2,70 @@ const fs = require('fs')
 const path = require('path')
 
 let inputPath = path.resolve(__dirname, "input.txt");
-let input = fs.readFileSync(inputPath).toString().split("\n").map(row => row.split('').map(Number))
 
 let p1 = puzzle1()
 let p2 = puzzle2()
 console.log(p1, p2)
 
 function puzzle1() {
-    let costs = makeArray(input[0].length, input.length)
-    let end = [input.length - 1, input[0].length - 1]
-    costs[end[0]][end[1]] = input[end[0]][end[[1]]]
-    for (let i = input.length - 1; i >= 0; i--) {
-        for (let j = input[0].length - 1; j >= 0; j--) {
-            if (costs[i][j] > -1) {
-                continue
-            }
-
-            let possible = getPossibleMoves([i, j])
-            let costsForPos = []
-            for (let pos of possible) {
-                if (costs[pos[0]][pos[1]] <= 0) {
-                    console.error(i, j, pos, costsForPos)
-                    throw 'unknown'
-                }
-
-                costsForPos.push(input[i][j] + costs[pos[0]][pos[1]])
-            }
-
-            costs[i][j] = Math.min(...costsForPos)
-        }
-    }
-
-    console.log(costs[0][0] - input[0][0])
+    let input = fs.readFileSync(inputPath).toString().split("\n").map(row => row.split('').map(Number))
+    return puzzle(input)
 }
 
 function puzzle2() {
-
+    let inputRaw = fs.readFileSync(inputPath).toString().split("\n").map(row => row.split('').map(Number))
+    let expanded = expandInput(inputRaw)
+    return puzzle(expanded)
 }
 
-function getPossibleMoves(pos, considerBackwards) {
+function puzzle(input) {
+    let costs = makeArray(input[0].length, input.length, Infinity)
+    let unvisited = makeArray(input[0].length, input.length, true)
+    costs[0][0] = 0
+    let current = [0, 0]
+    let prev
+
+    while (current !== undefined) {
+        step(costs, input, unvisited, current)
+        prev = [...current]
+        current = getNext(costs, unvisited)
+    }
+
+    return costs[input.length - 1][input[0].length - 1]
+}
+
+function getNext(costs, unvisited) {
+    let minCost = Infinity
+    let next = undefined
+    for (let i = 0; i < costs.length; i++) {
+        for (let j = 0; j < costs[0].length; j++) {
+            if (unvisited[i][j] && costs[i][j] < minCost) {
+                next = [i, j]
+                minCost = costs[i][j]
+            }
+        }
+    }
+
+    return next
+}
+
+function step(costs, input, unvisited, current) {
+    let toVisit = getPossibleMoves(current, input, unvisited)
+    for (let p of toVisit) {
+        if (input[p[0]][p[1]] <= 0 || input[p[0]][p[1]] > 9) {
+            throw p
+        }
+
+        let cost = input[p[0]][p[1]] + costs[current[0]][current[1]]
+        if (costs[p[0]][p[1]] > cost) {
+            costs[p[0]][p[1]] = cost
+        }
+    }
+
+    unvisited[current[0]][current[1]] = false
+}
+
+function getPossibleMoves(pos, input, unvisited) {
     let ret = []
     if (input[pos[0]][pos[1]+1]) {
         ret.push([pos[0],pos[1]+1])
@@ -50,13 +75,39 @@ function getPossibleMoves(pos, considerBackwards) {
         ret.push([pos[0] + 1,pos[1]])
     }
 
-    return ret
+    if (input[pos[0]][pos[1]-1]) {
+        ret.push([pos[0],pos[1]-1])
+    }
+
+    if (input[pos[0]-1]) {
+        ret.push([pos[0]-1,pos[1]])
+    }
+
+    return ret.filter(r => unvisited[r[0]][r[1]])
 }
 
-function makeArray(d1, d2) {
+function makeArray(d1, d2, fill) {
     var arr = [];
     for(let i = 0; i < d2; i++) {
-        arr.push(new Array(d1).fill(-1));
+        arr.push(new Array(d1).fill(fill));
     }
     return arr;
+}
+
+function expandInput(inputRaw) {
+    let n = inputRaw.length
+    let expanded = makeArray(n * 5, n * 5)
+    for (let k = 0; k < 5; k++) {
+        for (let l = 0; l < 5; l++) {
+            for (let i = 0; i < inputRaw.length; i++) {
+                for (let j = 0; j < inputRaw[0].length; j++) {
+                    let initial = inputRaw[i][j]
+                    let nextVal = 1 + ((initial + k + l - 1) % 9)
+                    expanded[i+(k*n)][j+(l*n)] = nextVal
+                }
+            }
+        }
+    }
+
+    return expanded
 }
